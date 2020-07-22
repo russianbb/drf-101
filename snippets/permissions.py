45ada9c0aa519d44b31180ea7751b11e8f7894c8
Bruno Borges Russian <bruno.russian.i@gmail.com>
2020-07-22 18:22:23 -0300

    DRF - Refatorando Tutorial 4

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -3,14 +3,14 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    Custo permission to only allow owners of an object to edit it
+    Custom permission to only allow owners of an object to edit it.
     """
-    
+
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allower to any request
-        # so we`ll always allow GET, HEAD or OPTIONS request
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
-        
-        # Write permissions are only allower to the owner of the snippet
+
+        # Write permissions are only allowed to the owner of the snippet.
         return obj.owner == request.user
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import include
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
@@ -8,3 +9,7 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
